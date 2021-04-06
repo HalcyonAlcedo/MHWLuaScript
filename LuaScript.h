48415b7218012476e660879b1934ba1727c5f64d
HalcyonAlcedo <zyc404@gmail.com>
2021-04-06 16:15:05 +0800

    修正写法

--- LuaScript.h
+++ LuaScript.h
@@ -999,28 +999,29 @@ static int System_Memory_SetAddressData(lua_State* pL) {
     string type = (string)lua_tostring(pL, 2);
     long long Ptr = 0;
     sscanf_s(ptr.c_str(), "%p", &Ptr, sizeof(long long));
-    switch (type)
-    {
-    case "int":
-        *(int*)(Ptr) = (int)lua_tointeger(pL, 3);
-        lua_pushboolean(pL, true);
-        break;
-    case "float":
-        *(float*)(Ptr) = (float)lua_tonumber(pL, 3)
-        lua_pushboolean(pL, true);
-        break;
-    case "bool":
-        *(bool*)(Ptr) = (bool)lua_toboolean(pL, 3)
-        lua_pushboolean(pL, true);
-        break;
-    case "byte":
-        *(char*)(Ptr) = (char)lua_tointeger(pL, 3)
-        lua_pushboolean(pL, true);
-        break;
-    default:
-        lua_pushboolean(pL, false);
-        break;
+    void* ptrAddress = (void*)Ptr;
+    if (ptrAddress != nullptr) {
+        if (type == "int") {
+            *(int*)(Ptr) = (int)lua_tointeger(pL, 3);
+            lua_pushboolean(pL, true);
+        }
+        else if (type == "float") {
+            *(float*)(Ptr) = (float)lua_tonumber(pL, 3);
+            lua_pushboolean(pL, true);
+        }
+        else if (type == "bool") {
+            *(bool*)(Ptr) = (bool)lua_toboolean(pL, 3);
+            lua_pushboolean(pL, true);
+        }
+        else if (type == "byte") {
+            *(char*)(Ptr) = (char)lua_tointeger(pL, 3);
+            lua_pushboolean(pL, true);
+        }
+        else
+            lua_pushboolean(pL, false);
     }
+    else
+        lua_pushboolean(pL, false);
     return 1;
 }
 #pragma endregion
